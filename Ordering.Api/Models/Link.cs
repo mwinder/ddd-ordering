06a31dbcaf3973243591a920232401f07d06664b
Matthew Winder <@mwinder>
2015-07-01 12:45:15 +0100

    Added Templated link property

--- Ordering.Api/Models/Link.cs
+++ Ordering.Api/Models/Link.cs
@@ -1,17 +1,22 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Ordering.Api.Models
 {
     public class Link
     {
-        public Link(Uri href)
+        public Link(Uri href, bool templated = false)
         {
             //Rel = rel;
             Href = href;
+            Templated = templated;
         }
 
         //public string Rel { get; private set; }
 
         public Uri Href { get; private set; }
+
+        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
+        public bool Templated { get; private set; }
     }
 }
